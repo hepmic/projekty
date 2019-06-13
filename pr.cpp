e10c645bf454267f20c57a17f16611beb093c712
hepmic <51704161+hepmic@users.noreply.github.com>
2019-06-14 01:17:03 +0200

    Update pr.cpp
    końcowy projekt

--- pr.cpp
+++ pr.cpp
@@ -31,7 +31,7 @@ public:
 private:
   static string target;        //cel jest ukryty
 };
-string selection::target = "Chromosome representationChromosome representation";
+string selection::target = "Chromosome representation";
 
 bool less_fit(string const& s1, string const& s2)
 {
@@ -57,10 +57,21 @@ string mutate(string parent, double MutationRate)
     }
   return parent;
 }
+string mutate_krz(string parent_1, string parent_2){
+	string result = parent_1;
+	int sum1 = (-1) * selection::fitness(parent_1), sum2 = (-1) * selection::fitness(parent_2);
+	double sum = sum1 / (sum1 + sum2);
+	for(int i = 0; i < parent_1.length(); i++)
+		{
+			if(rand()/(RAND_MAX + 1.0) < sum)
+				result[i] = parent_2[i];
+		}
+	return result;
+}
 
 int main()
 {
-  int const C = 100;
+  int const C = 250;
   srand(time(0));
   string parent;
   for (int i = 0; i < selection::target_length(); ++i)
@@ -68,19 +79,28 @@ int main()
     parent += AllowedChars[rand() % AllowedChars.length()];      //losowanie poczatkowego stringa
   }
   int const InitialFitness = selection::fitness(parent);
-
+	string parent_old[5] = {parent, parent, parent, parent, parent};
   for(int fitness = InitialFitness, j = 1;
       fitness < 0;
       fitness = selection::fitness(parent), j++)
   {
-    cout << "Generacja nr:" << j << "\t" << parent << ": " << fitness << "\n";
-    double const MutationRate = 0.03 + (0.9*fitness)/InitialFitness;
+    cout << "Generacja nr:" << j << "   \t" << parent << ": " << fitness << "\n";
+    const double MutationRate = 0.04 + (0.99*fitness)/InitialFitness;
     vector<string> childs;
-    childs.reserve(C+1);
+    childs.reserve(C + 50);
     childs.push_back(parent);
     for (int i = 0; i < C; ++i)
       childs.push_back(mutate(parent, MutationRate));
+      
+    for(int i = 0; i < 5; i++)
+		childs.push_back(parent_old[i]);
+	for(int k = 0; k < 5; k++)
+		for(int l = k + 1; l < 5; l++)
+			childs.push_back(mutate_krz(parent_old[k], parent_old[l]));
+			
     parent = *max_element(childs.begin(), childs.end(), less_fit);
+    for(int i = 0; i < 5; i++)
+		parent_old[i] = childs[i];
   }
-  cout << "final string: " << parent << "\n";
+  cout << "Wynik: " << parent << "\n";
 }
